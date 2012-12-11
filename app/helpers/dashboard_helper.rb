315d479de5682f6e735475c9f35e3555b900cd2c
zoeoberon <joan@zoeoberon.com>
2012-12-11 12:35:50 -0600

    update dashboard and README text, create shared comments and open issues lists

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,8 +1,22 @@
 module DashboardHelper
 
   require 'bundler'
-  
   def bundled_gems
     Bundler.load.specs
   end
+  
+  def convert_list_to_ul_block(rdocname)
+    # needed to share lists between dashboard and github README
+    # convert simple '-' rdoc list to a <ul> block
+    filename = Rails.root.join('doc', rdocname)
+    if File.exist?(filename) and File.file?(filename)
+      html = "<ul>"
+      File.read(filename).each_line do |item|
+        html += (item.gsub(/^-/,'<li>') + '</li><br />') if item =~ /^-/
+      end
+      return html + "</ul>"
+    end
+    ""
+  end
+
 end
