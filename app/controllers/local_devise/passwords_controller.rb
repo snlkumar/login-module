5df09a3bc17e63660356686df3bc71724c121a41
zoeoberon <joan@zoeoberon.com>
2012-12-11 10:03:33 -0600

    error message when no email address for username entered on forgot password form

--- app/controllers/local_devise/passwords_controller.rb
+++ app/controllers/local_devise/passwords_controller.rb
@@ -2,10 +2,14 @@ class LocalDevise::PasswordsController < Devise::PasswordsController
 
   def create
     user_email = fetch_email(resource_params[:username]) if resource_params[:username]
-    resource_params = {:email => user_email} if user_email
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    unless successfully_sent?(resource)
-      flash[:error] = t(:unable_send_password_email, :scope => 'devise.failure') if is_navigational_format?
+    if user_email
+      resource_params = {:email => user_email}
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+      unless successfully_sent?(resource)
+        flash[:error] = t(:unable_send_password_email, :scope => 'devise.failure') if is_navigational_format?
+      end
+    else
+        flash[:error] = t(:unable_to_find_users_email, :scope => 'devise.failure') if is_navigational_format?
     end
     redirect_to dashboard_path
   end
