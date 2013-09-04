1734c11252256b8abecfd35d8e83975acf37d66e
zoeoberon <joan@zoeoberon.com>
2013-09-04 16:01:12 +0000

    move secret_token to private.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myinfo::Application.config.secret_token = '3d8d1ad715bc7511ea0620a2ed35cbaed28ac05aa968c4c33bdb52c0e62eeaea3c097263c3b4a98ad402c1cf73aa1584058d770e40eb6a76a33258f7f5213545'
+Myinfo::Application.config.secret_token = PRIVATE_DATA['secret_token']
