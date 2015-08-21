36bfcf04c80438a08e3cfd638780539c1e695878
sunil <sunil.kumar.14@netsolutionsindia.com>
2015-08-21 14:14:07 +0530

    fix the issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Myinfo::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => PRIVATE_DATA['url_host'] }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-User.find_or_create_by_username(:username => "admin", :email => 'admin@zoeoberon.com', :role => 'admin', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "molly", :email => 'molly@zoeoberon.com', :role => 'manager', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "fred", :email => 'fred@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "guest", :email => 'guest@zoeoberon.com', :role => 'guest', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "felix", :email => 'felix@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "suzy", :email => 'suzy@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "jane", :email => 'jane@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
-User.find_or_create_by_username(:username => "darcy", :email => 'darcy@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "admin", :email => 'admin@zoeoberon.com', :role => 'admin', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "molly", :email => 'molly@zoeoberon.com', :role => 'manager', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "fred", :email => 'fred@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "guest", :email => 'guest@zoeoberon.com', :role => 'guest', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "felix", :email => 'felix@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "suzy", :email => 'suzy@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "jane", :email => 'jane@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
+User.create(:username => "darcy", :email => 'darcy@zoeoberon.com', :role => 'user', :password => '123123', :password_confirmation => '123123')
